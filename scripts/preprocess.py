740d4a9631fa340f04e4d5de6db73b55fdba654f
beckdaniel <daniel.e.beck@gmail.com>
2015-10-03 15:02:54 +0100

    preprocess stuff

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -2,6 +2,17 @@ import numpy as np
 import nltk
 from nlkt.tokenize import wordpunct_tokenize
 
+# Some constants. The weird order in the EMO list
+# is to facilitate the coregionalization matrix plotting
+EMOS = ['sadness','fear','anger','disgust','surprise','joy']
+EMO_DICT = {}
+EMO_DICT['anger'] = 1
+EMO_DICT['disgust'] = 2
+EMO_DICT['fear'] = 3
+EMO_DICT['joy'] = 4
+EMO_DICT['sadness'] = 5
+EMO_DICT['surprise'] = 6
+
 
 def preprocess_sent(sent, lemmatizer):
     """
@@ -36,4 +47,66 @@ def read_sents_file(sents_file, train, test):
     return train_sents, test_sents
 
 
+def build_word_dict(train_sents):
+    """
+    Return a dict with words mapped to their indices.
+    Words that appear only on the test set will
+    not appear here (and therefore won't count
+    as features in the BOW model).
+    """
+    word_dict = {}
+    for sent in train_sents.values():
+        for word in sent:
+            if word not in word_dict:
+                word_dict[word] = len(word_dict)
+    return word_dict
+
+
+def build_feat_vectors(train_sents, train_labels, test_sents,
+                       test_labels, word_dict):
+    """
+    Build the feature vectors for train and test sets, 
+    which encode a BOW representation for each sentence.
+    """
+    train_feats = np.zeros(shape=(len(train_sents), len(word_dict)))
+    for i,row in enumerate(train_labels):
+        sent_id = row[0]
+        for word in train_sents[sent_id]:
+            train_feats[i][word_dict[word]] += 1
+    test_feats = np.zeros(shape=(len(test_sents), len(word_dict)))
+    for i,row in enumerate(test_labels):
+        sent_id = row[0]
+        for word in test_sents[sent_id]:
+            if word in word_dict:
+                test_feats[i][word_dict[word]] += 1
+    return train_feats, test_feats
+
+
+def build_data(sents_file, labels_file, train, test):
+    """
+    Build and preprocess the data into a suitable format
+    for the learning algorithms. The format is a dictionary,
+    where the key is the emotion and the value is a matrix where
+    the rows are the BOW vectors concatenated with the emotion label
+    in the last column. It's very inefficient since it replicates
+    the BOW vectors for each emotion but GPy needs that for the
+    ICM models. Also, the dataset is small =P.
+    """
+    labels = np.loadtxt(labels_file)
+    train_labels = labels[:train]
+    test_labels = labels[-test:]
+    train_sents, test_sents = read_sents_file(sents_file, train, test)
+    word_dict = build_word_dict(train_sents)
+    train_feats, test_feats = build_feat_vectors(train_sents, train_labels,
+                                                 test_sents, test_labels,
+                                                 word_dict)
+    train_data = {}
+    test_data = {}
+    for emo in EMOS:
+        emo_id = EMO_DICT[emo]
+        train_data[emo] = np.concatenate((train_feats, 
+                                          train_labels[:,emo_id:emo_id+1]), axis=1)
+        test_data[emo] = np.concatenate((test_feats, 
+                                         test_labels[:,emo_id:emo_id+1]), axis=1)
+    return train_data, test_data
     
